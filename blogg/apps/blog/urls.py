ef5fe4a1c54c8b1b8e7b749fd00887753e093e22
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-09-06 12:27:50 +0600

    details blogg added

--- blogg/apps/blog/urls.py
+++ blogg/apps/blog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import homepage
+
+from .views import homepage, post_detail
 
 urlpatterns = [
    path('', homepage, name='homepage'),
+   path('post/<int:post_id>/', post_detail, name='post_detail'),
 ]
--- blogg/apps/blog/views.py
+++ blogg/apps/blog/views.py
@@ -6,3 +6,7 @@ def homepage(request):
     blog = Post.objects.filter(published = True)
 
     return render(request, 'homepage.html', {'blog': blog})
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'post_detail.html', {'post': post})
