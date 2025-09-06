f91b5e171253e416411075cf86922595a481c92a
Md Oshama Bin Nur <mdobns@hotmail.com>
2025-09-06 12:44:15 +0600

    edit function added

--- blogg/apps/blog/urls.py
+++ blogg/apps/blog/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 
-from .views import homepage, post_detail
+from .views import edit_post_save, homepage, post_detail ,edit_post
 
 urlpatterns = [
    path('', homepage, name='homepage'),
    path('post/<int:post_id>/', post_detail, name='post_detail'),
+   path('post/<int:post_id>/edit/', edit_post, name='edit_post'),
+   path('post/<int:post_id>/edit/save/', edit_post_save, name='edit_post_save'),
 ]
--- blogg/apps/blog/views.py
+++ blogg/apps/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 
 # Create your views here.
@@ -10,3 +10,17 @@ def homepage(request):
 def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     return render(request, 'post_detail.html', {'post': post})
+
+
+def edit_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'edit_post.html', {'post': post})
+
+def edit_post_save(request, post_id):
+    post = Post.objects.get(id=post_id)
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('post_detail', post_id=post.id)
+    return render(request, 'post_detail', post_id=post.id)
